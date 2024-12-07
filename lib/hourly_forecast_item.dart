714395519352054a3aebaa68a726cd7458116d0e
Mehedi Hasan Tarek <mhtarek@live.com>
2024-12-07 19:57:40 +0600

    Update
    Updating the code.

--- lib/hourly_forecast_item.dart
+++ lib/hourly_forecast_item.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 class HourlyForecastItem extends StatelessWidget {
-  const HourlyForecastItem({super.key});
+  final String time;
+  final String temperature;
+  final IconData icon;
+
+  const HourlyForecastItem(
+      {super.key,
+      required this.time,
+      required this.temperature,
+      required this.icon});
 
   @override
   Widget build(BuildContext context) {
@@ -13,24 +21,22 @@ class HourlyForecastItem extends StatelessWidget {
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
         ),
-        child: const Column(
+        child: Column(
           children: [
             Text(
-              '03.00',
-              style: TextStyle(
+              time,
+              style: const TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 8),
+            const SizedBox(height: 8),
             Icon(
-              Icons.cloud,
+              icon,
               size: 32,
             ),
-            SizedBox(height: 8),
-            Text(
-              '320.12',
-            ),
+            const SizedBox(height: 8),
+            Text(temperature),
           ],
         ),
       ),
--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -49,7 +49,7 @@ class WeatherScreen extends StatelessWidget {
                       child: Column(
                         children: [
                           Text(
-                            '300 F',
+                            '300K',
                             style: TextStyle(
                               fontSize: 32,
                               fontWeight: FontWeight.bold,
@@ -87,11 +87,31 @@ class WeatherScreen extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
-                  HourlyForecastItem(),
-                  HourlyForecastItem(),
-                  HourlyForecastItem(),
-                  HourlyForecastItem(),
-                  HourlyForecastItem(),
+                  HourlyForecastItem(
+                    time: '00.00',
+                    icon: Icons.cloud,
+                    temperature: '301.22',
+                  ),
+                  HourlyForecastItem(
+                    time: '03.00',
+                    icon: Icons.sunny,
+                    temperature: '300.52',
+                  ),
+                  HourlyForecastItem(
+                    time: '06.00',
+                    icon: Icons.cloud,
+                    temperature: '302.22',
+                  ),
+                  HourlyForecastItem(
+                    time: '09.00',
+                    icon: Icons.sunny,
+                    temperature: '300.12',
+                  ),
+                  HourlyForecastItem(
+                    time: '12.00',
+                    icon: Icons.cloud,
+                    temperature: '304.12',
+                  ),
                 ],
               ),
             ),
@@ -106,9 +126,21 @@ class WeatherScreen extends StatelessWidget {
             const Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                AdditionalInfoItem(),
-                AdditionalInfoItem(),
-                AdditionalInfoItem(),
+                AdditionalInfoItem(
+                  icon: Icons.water_drop,
+                  label: 'Humidity',
+                  value: '91',
+                ),
+                AdditionalInfoItem(
+                  icon: Icons.air,
+                  label: 'Windy Speed',
+                  value: '7.5',
+                ),
+                AdditionalInfoItem(
+                  icon: Icons.beach_access,
+                  label: 'Pressure',
+                  value: '1000',
+                ),
               ],
             ),
           ],
